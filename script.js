318a704bcd4e93f32d856f2234c9eab6ddd5dcc3
ritik mehrotra <ritik1.mehrotra@gmail.com>
2022-11-12 18:10:49 +0000

    Added a sound while changing the panel in JS

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const panels = document.querySelectorAll('.panel');
+var audio = new Audio("https://www.mboxdrive.com/pre_650i%20(2).mp3.asset.1449924565635%20(mp3cut.net).mp3");
 panels.forEach(panel=>{
   panel.addEventListener('click',()=>{
     removeActiveClass();  
     panel.classList.add('active');
-    
+    audio.play();
   })
 })
 
